f910ab17b5872f798b0ce087a9113c1aff89172d
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2023-09-29 17:39:28 -0700

    async-await lessons

--- asynchronous-programming-with-javascript/4- Exploring Async-Await/2-convert-promise-handling-to-async-await/start/js/async-await.js
+++ asynchronous-programming-with-javascript/4- Exploring Async-Await/2-convert-promise-handling-to-async-await/start/js/async-await.js
@@ -4,7 +4,33 @@ const peopleList = document.getElementById('people');
 const btn = document.querySelector('button');
 
 // Handle all fetch requests
+async function getJSON(url) {
+  try {
+    const response = await fetch(url);
+    return await response.json();
+  } catch (error) {
+    throw error;
 
+  }
+
+}
+
+
+async function getPeopleInSpace(url){
+  const peopleJSON = await getJSON(url);
+
+  const profiles = peopleJSON.people.map( async (person) => {
+    const craft = person.craft;
+    const profileJSON = await getJSON(wikiUrl + person.name);
+
+    return {...profileJSON, craft}
+  });
+
+  return Promise.all(profiles);
+  // return url;
+}
+
+console.log(getPeopleInSpace(astrosUrl))
 
 // Generate the markup for each profile
 function generateHTML(data) {
@@ -31,7 +57,14 @@ function generateHTML(data) {
   });
 }
 
-btn.addEventListener('click', (event) => {
+btn.addEventListener('click',(event) => {
   event.target.textContent = "Loading...";
 
+  getPeopleInSpace(astrosUrl)
+    .then(generateHTML)
+    // .catch( e => {
+    //   peopleList.innerHTML = '<h3>Something went wrong</h3>'
+    //   console.error(e)
+    // })
+    .finally( () =>  event.target.remove() )
 });
