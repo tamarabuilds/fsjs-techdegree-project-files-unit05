7a0839d673d943b7279205639ca578e846a6d243
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2023-10-02 09:38:23 -0700

    practice api fetch

---
+++ practice-fetch-api/js/app.js
@@ -0,0 +1,13 @@
+//1. Create an async function called getCountries
+//  - retrieve the name, capital, population and flags for all countries.
+//  - Convert the response to JSON.
+//  - pass the data to the displayCountries function.
+//  - Catch any errors and log them to the console.
+
+//2. Create a displayCountries function that takes in an array of countries.
+//  - Loop over the array of countries.
+//      - Create a div for each country.
+//      - Add the country name and flag to the div with the provided HTML structure.
+//      - Add the created div to the `.countries` container element.
+
+//3. Call the getCountries function.
